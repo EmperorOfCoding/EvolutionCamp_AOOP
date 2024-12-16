e02a7701b3171a2271da38d23707a6e9f633723c
EmperorOfCoding <victorameno@hotmail.com>
2024-12-16 14:29:47 -0300

    Atividade 3 Finalizada

---
+++ EvolutionCamp/Atividade3/script.js
@@ -0,0 +1,21 @@
+
+let num1 = "25.5";
+let num2 = "30.7";
+
+let num1_inteiro = parseInt(num1);
+let num2_float = parseFloat(num2);
+
+let soma = num1_inteiro + num2_float;
+let soma_round = Math.round(soma);
+let soma_floor = Math.floor(soma);
+let soma_ceil = Math.ceil(soma);
+
+
+var numero_aleatorio = Math.round(Math.random(numero_aleatorio) * 10);
+var numero_elevado = Math.pow(numero_aleatorio, 2);
+
+console.log(numero_aleatorio);
+console.log(numero_elevado);
+
+
+
