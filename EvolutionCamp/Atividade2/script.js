4eeb4c797d2aebe0901b78056c705ade514c82b9
EmperorOfCoding <victorameno@hotmail.com>
2024-12-16 11:45:15 -0300

    Exercicio2 finalizado

---
+++ EvolutionCamp/Atividade2/script.js
@@ -0,0 +1,27 @@
+//Exercicio1
+
+let numero1 = prompt("Digite o primeiro número: ");
+let numero2 = prompt("Digite o segundo número: ");
+
+alert(`A soma entre numero1 e numero2 é: ${numero1 + numero2}`);
+alert(`A subtracao entre numero1 e numero2 é: ${numero1 - numero2}`);
+alert(`A subtracao entre numero1 e numero2 é: ${numero1 * numero2}`);
+alert(`A subtracao entre numero1 e numero2 é: ${numero1 / numero2}`);
+
+//Exercicio2
+let a = 6;
+let b = 2;
+
+a = b;
+console.log(a);
+
+
+//Exercicio3
+let name = "Victor Martins";
+let age = 19;
+let like = true;
+
+console.log(`Hello, my name is ${name}.`);
+console.log(`I am ${age} years old.`);
+console.log(`It is ${like} that I like programming`);
+
